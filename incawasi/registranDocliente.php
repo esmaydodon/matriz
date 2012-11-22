2175622103225511c59f2ad6c6273fd75f16df37
Marlon Martos Quiroz <marlonmartos@hotmail.com>
2012-11-22 17:58:41 -0500

    modificado desde  web

--- incawasi/registranDocliente.php
+++ incawasi/registranDocliente.php
@@ -1,4 +1,7 @@
 <?php 
+session_start();
+extract($_REQUEST);
+//$dedonde=$_GET["dedo"];
 include("func/funciones.php");
 #ID producto, precio cantidad defaul 1 ($idproducto,$precio,$cantidad) ásar x js
 $idp = $_POST['idproduc']; 
@@ -20,7 +23,26 @@ $consulta = "INSERT INTO usuarios( idusuarios,
 								  '$ema_comprador',
 								  '$tipo_usuario',
 								  '$pai_usuario')"  ;
-$resultado = dime($consulta) or die(mysql_error());$last_usuario_id = mysql_insert_id();
+$resultado = dime($consulta) or die(mysql_error());
+$last_usuario_id = mysql_insert_id();
+//session- junto con dedonde
+$cliente1 = dime("'select * from usuarios where idusuarios =".$last_usuario_id."'");
+$filaCliente=  mysql_fetch_array($cliente1);
+if(isset($_SESSION['cliente'])) 
+$cliente=$_SESSION['cliente'];
+$cliente[md5($id)]=array('identificador'=>md5($id), 
+			'idusuario'=>$id,
+			'nombre_apellidos_usu'=>$filaCliente['nombre_apellidos_usu'],
+			'email_usuario'=>$filaCliente['email_usuario'],
+			  'telefono_usuario'=>$filaCliente['telefono_usuario']
+			); 
+$_SESSION['cliente']=$cliente; 
+/*if (!isset($dedonde)) {
+    header("Location:vender.php?".SID); 
+}else{
+    header("Location:vender.php?".SID);  
+}*/
+//sesion end
 $consulta2 ="INSERT INTO compras (usuarios_idusuarios,compra_total,compra_fecha)  VALUES  ($last_usuario_id,'$monto','$fecha')";
 $resultado2 = dime($consulta2) or die(mysql_error());$last_compra_id = mysql_insert_id();
 #comenzamos a registrar detalle de orden / compra veremos eso de  estado = :-)
